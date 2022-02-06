24ffb3744f236f5a6c47277bc6c98dd500e8afdd
gereza7 <gereza7@gmail.com>
2022-02-06 12:43:19 +0000

    axios ready

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import './main.css'
 
 createApp(App)
@@ -11,5 +12,5 @@ import Vue from 'vue'
 
 import VueBottomNavigation from 'bottom-navigation-vue'
 import router from './router'
-
+Vue.use(VueAxios, axios)
 Vue.use(VueBottomNavigation)
